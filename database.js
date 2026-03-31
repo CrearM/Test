bdc6a5427b8e71227f30de3f08c0524640473f8b
Mike Olorunfemi <35736864+CrearM@users.noreply.github.com>
2026-03-31 08:09:00 -0400

    Add/update database.js

---
+++ database.js
@@ -0,0 +1,4 @@
+// Project: 21 | Author: 9e9db132-363d-4411-9541-8836de8a9da6 | Generated: 2026-03-30T13:54:31.462Z | Build: #1
+CREATE TABLE IF NOT EXISTS learned_items (
+  item_id TEXT NOT NULL
+);
